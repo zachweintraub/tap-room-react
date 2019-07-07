919ac02154d973c4d60e0f5f973f50814b9ba561
Zach Weintraub <zachweintraub@gmail.com>
2019-07-07 12:40:36 -0700

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Router, Switch, Link } from 'react-router-dom';
+
+
+function App() {
+    return (
+        <div>
+            <h1>Template Project Works</h1>
+        </div>
+    );
+}
+
+export default App;
