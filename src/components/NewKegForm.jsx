d9d4b02890da128cfe184802ddf29635a7881b0f
Zach Weintraub <zachweintraub@gmail.com>
2019-07-12 14:03:52 -0700

    implement save new keg functionality

--- src/components/NewKegForm.jsx
+++ src/components/NewKegForm.jsx
@@ -3,8 +3,20 @@ import PropTypes from 'prop-types';
 
 function NewKegForm(props) {
 
-    function onSaveNewKeg(e, newKeg) {
-        e.preventDefault();
+    let _newBrewery;
+    let _newName;
+    let _newAbv;
+    let _newPrice;
+
+    function onSaveNewKeg(event) {
+        event.preventDefault();
+        let newKeg = {
+            name: _newName.value,
+            brewer: _newBrewery.value,
+            abv: _newAbv.value,
+            pintPrice: _newPrice.value,
+            remainingPints: 124
+        };
         props.onSaveNewKeg(newKeg);
     }
 
@@ -14,11 +26,11 @@ function NewKegForm(props) {
 
     return(
         <div>
-            <form onSubmit={() => {onSaveNewKeg(event, 'test');}}>
-                <input placeholder='Brewery'/>
-                <input placeholder='Name'/>
-                <input type='number' placeholder='ABV'/>
-                <input type='number' placeholder='Price'/>
+            <form onSubmit={onSaveNewKeg}>
+                <input placeholder='Brewery' ref={(input) => {_newBrewery = input;}}/>
+                <input placeholder='Name' ref={(input) => {_newName = input;}}/>
+                <input type='number' placeholder='ABV' ref={(input) => {_newAbv = input;}}/>
+                <input type='number' placeholder='Price' ref={(input) => {_newPrice = input;}}/>
                 <button type='submit'>Save</button>
             </form>
             <button onClick={onCancelAddNewKeg}>Cancel</button>
